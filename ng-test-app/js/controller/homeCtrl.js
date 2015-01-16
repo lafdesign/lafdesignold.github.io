132296512a4db25201e19b3391c7088863d9f924
jordan lafland <jordantlafland@gmail.com>
2015-01-16 13:01:28 -0500

    ng test

--- ng-test-app/js/controller/homeCtrl.js
+++ ng-test-app/js/controller/homeCtrl.js
@@ -1,8 +1,7 @@
 angular
 	.module('app')
 	.controller('homeCtrl', ['$scope', function($scope) {
-		$scope.title ="Home";
-		$scope.friends = friends;
+		$scope.title = "Home";
 		$scope.items = ['home', 'about', 'contact'];
 		$scope.selectedValue = 'home';
 	}]);
