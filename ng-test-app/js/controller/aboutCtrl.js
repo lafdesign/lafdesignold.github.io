d7b6f9c91fbbb500d43a12c2ccde20b376b2dad4
jordan lafland <jordantlafland@gmail.com>
2015-01-16 13:13:13 -0500

    ng test

--- ng-test-app/js/controller/aboutCtrl.js
+++ ng-test-app/js/controller/aboutCtrl.js
@@ -3,5 +3,5 @@ angular
 	.controller('aboutCtrl', ['$scope', function($scope) {
 		$scope.title ="About";
 		$scope.items = ['thing1', 'thing2', 'thing3'];
-		$scope.projects = ['#','#','#']
+		$scope.projects = ['#','#','#'];
 	}]);
