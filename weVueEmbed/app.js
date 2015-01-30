99cc6a86a807cdf423647ac2495f74cb83d8e6df
jordan lafland <jordantlafland@gmail.com>
2015-01-30 16:07:32 -0500

    test

--- weVueEmbed/app.js
+++ weVueEmbed/app.js
@@ -1,13 +1,18 @@
 (function() {
-  var app = angular.module('weVueFeed', []);
+  var app = angular.module('gemStore', []);
 
-  app.controller('embedFeedController', function() {
-    this.products = feeds;
+  app.controller('StoreController', function() {
+    this.products = gems;
   });
 
-  var feeds = [{
+  var gems = [{
     name: 'Azurite',
-    description: "#",
+    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+    shine: 8,
+    price: 110.50,
+    rarity: 7,
+    color: '#CCC',
+    faces: 14,
     images: [
       "images/img5.png",
       "images/img6.png",
@@ -15,7 +20,12 @@
     ]
   }, {
     name: 'Bloodstone',
-    description: "#",
+    description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+    shine: 9,
+    price: 22.90,
+    rarity: 6,
+    color: '#EEE',
+    faces: 12,
     images: [
       "images/img9.png",
       "images/img8.png",
@@ -23,10 +33,15 @@
     ]
   }, {
     name: 'Zircon',
-    description: "#",
+    description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+    shine: 70,
+    price: 1100,
+    rarity: 2,
+    color: '#000',
+    faces: 6,
     images: [
       "images/img3.png",
-      "images/img4.png",
+      "images/img6.png",
       "images/img7.png"
     ]
   }];
