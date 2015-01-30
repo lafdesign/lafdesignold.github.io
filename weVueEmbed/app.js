05ab29c10ad8346c74802af85cf4bcfa295922af
jordan lafland <jordantlafland@gmail.com>
2015-01-30 16:22:25 -0500

    test

--- weVueEmbed/app.js
+++ weVueEmbed/app.js
@@ -6,39 +6,24 @@
   });
 
   var gems = [{
-    name: 'Azurite',
-    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
-    shine: 8,
-    price: 110.50,
-    rarity: 7,
-    color: '#CCC',
-    faces: 14,
+    name: 'Anna',
+   
     images: [
       "images/img5.png",
       "images/img6.png",
       "images/img7.png"
     ]
   }, {
-    name: 'Bloodstone',
-    description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
-    shine: 9,
-    price: 22.90,
-    rarity: 6,
-    color: '#EEE',
-    faces: 12,
+    name: 'Beth',
+  
     images: [
       "images/img9.png",
       "images/img8.png",
       "images/img7.png"
     ]
   }, {
-    name: 'Zircon',
-    description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-    shine: 70,
-    price: 1100,
-    rarity: 2,
-    color: '#000',
-    faces: 6,
+    name: 'Zach',
+    
     images: [
       "images/img3.png",
       "images/img6.png",
