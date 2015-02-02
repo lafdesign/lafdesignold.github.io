2ea2eda4cc08798343a42c06a4e35b578a2eaf94
jordan lafland <jordantlafland@gmail.com>
2015-02-02 00:39:45 -0500

    test

--- WeVue/js/controllers/galleryCtrl.js
+++ WeVue/js/controllers/galleryCtrl.js
@@ -1,6 +1,6 @@
 angular
 	.module('app')
-	.controller('GalleryCtrl', ['$scope', function($scope) {
+	.controller('GalleryController', ['$scope', function($scope) {
 		$scope.title = "Gallery";
 		$scope.items = ['home','gallery','contact'];
 	}]);
--- WeVue/js/controllers/homeCtrl.js
+++ WeVue/js/controllers/homeCtrl.js
@@ -1,6 +1,6 @@
 angular
 	.module('app')
-	.controller('HomeCtrl', ['$scope', 'events', function($scope, friends) {
+	.controller('HomeController', ['$scope', 'events', function($scope, friends) {
 		$scope.title = "WeVue";
 		$scope.events = events;
 		$scope.items = ['home','gallery','contact'];
