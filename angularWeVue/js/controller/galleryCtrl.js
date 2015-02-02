5c5d7bb58229a4ab43ff330d72a14e5d1248ff48
jordan lafland <jordantlafland@gmail.com>
2015-02-02 05:17:59 -0500

    test

--- angularWeVue/js/controller/galleryCtrl.js
+++ angularWeVue/js/controller/galleryCtrl.js
@@ -1,17 +1,8 @@
 angular
 	.module('app')
-	.controller('galleryCtrl', function ($scope, $http){
+	.controller('galleryCtrl',function($scope, $http){
 			$http.get('http://wevue.com/api/v1/events/335641975/media.json').success(function(data){
 				$scope.signatures= data;
 			});
-		})
-
-	.directive('mySlideshow', function () {
-	return {
-	   restrict: 'AC',
-      link: function (scope, element, attrs) {
-		var config = angular.extend({
-          slides: '.slide'
-        }
-	};
-});
+		});
+	
