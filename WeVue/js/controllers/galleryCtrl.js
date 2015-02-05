911460f87672b770d2bbde387297d17bf21b7adf
jordan lafland <jordantlafland@gmail.com>
2015-02-04 22:42:03 -0500

    test
    test

--- WeVue/js/controllers/galleryCtrl.js
+++
@@ -1,6 +0,0 @@
-angular
-	.module('app')
-	.controller('GalleryController', ['$scope', function($scope) {
-		$scope.title = "Gallery";
-		$scope.items = ['home','gallery','contact'];
-	}]);
--- WeVue/js/controllers/homeCtrl.js
+++
@@ -1,7 +0,0 @@
-angular
-	.module('app')
-	.controller('HomeController', ['$scope', 'events', function($scope, friends) {
-		$scope.title = "WeVue";
-		$scope.events = events;
-		$scope.items = ['home','gallery','contact'];
-	}]);
