ac0f2e60c41d3baf6894eab620e923a9d2c4743b
jordan lafland <jordantlafland@gmail.com>
2015-02-07 10:59:24 -0500

    initial push

--- angularWeVue/js/controller/galleryCtrl.js
+++
@@ -1,9 +0,0 @@
-angular
-	.module('app')
-	.controller('galleryCtrl',function($scope, $http){
-			$http.get('http://wevue.com/api/v1/events/335641975/media.json').success(function(data){
-				$scope.signatures= data;
-			});
-			
-		});
-	
--- angularWeVue/js/controller/homeCtrl.js
+++
@@ -1,7 +0,0 @@
-angular
-	.module('app')
-	.controller('homeCtrl',function($scope, $http){
-			$http.get('http://wevue.com/api/v1/events.json').success(function(data){
-			$scope.feeds = data;
-			});
-		});
