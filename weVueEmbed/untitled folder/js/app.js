bf086933619fc4cbc76131e09bff79222539397e
jordan lafland <jordantlafland@gmail.com>
2015-02-17 15:56:17 -0500

    test

--- weVueEmbed/untitled folder/js/app.js
+++ weVueEmbed/untitled folder/js/app.js
@@ -11,20 +11,5 @@ angular
 			controller: 'homeCtrl'	
 			
 		})
-			.state('gallery',{
-			url: '/gallery',
-			templateUrl: 'template/gallery.html',	
-			controller:'galleryCtrl'
-			resolve:{
-				signitures['$http', function($http){
-					return $http.get('http://wevue.com/api/v1/events/335641975/media.json').then(function(response) {
-						return response.data;
-					})
-				}]
-			}
-		})
-			.state('menu',{
-			url: '/menu',
-			templateUrl: 'template/menu.html'	
-		})
+			
 	}])
