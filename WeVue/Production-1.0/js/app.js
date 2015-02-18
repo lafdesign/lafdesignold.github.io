4bc811b09322283f82dec00d9de9a117d235cdc6
jordan lafland <jordantlafland@gmail.com>
2015-02-17 21:59:16 -0500

    test

--- WeVue/Production-1.0/js/app.js
+++ WeVue/Production-1.0/js/app.js
@@ -1,6 +1,41 @@
 
 
-
+(function() {
+            var quotes = $(".quotes");
+            var quoteIndex = -1;
+            function showNextQuote() {
+                ++quoteIndex;
+                quotes.eq(quoteIndex % quotes.length)
+                    .fadeIn(2000)
+                    .delay(1000)
+                    .fadeOut(2000, showNextQuote);
+            }
+            showNextQuote();
+        })();
+(function() {
+            var quotes = $(".quotes2");
+            var quoteIndex = -1;
+            function showNextQuote() {
+                ++quoteIndex;
+                quotes.eq(quoteIndex % quotes.length)
+                    .fadeIn(2000)
+                    .delay(1000)
+                    .fadeOut(2000, showNextQuote);
+            }
+            showNextQuote();
+        })();
+(function() {
+            var quotes = $(".showcase-img");
+            var quoteIndex = -1;
+            function showNextQuote() {
+                ++quoteIndex;
+                quotes.eq(quoteIndex % quotes.length)
+                    .fadeIn(2000)
+                    .delay(1000)
+                    .fadeOut(2000, showNextQuote);
+            }
+            showNextQuote();
+        })();
 
 
 onScrollInit( $('.os-animation') );
