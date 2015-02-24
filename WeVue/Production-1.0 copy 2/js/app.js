5e9618c6f77ef0af7582fb43b5922193ded8c5ca
jordan lafland <jordantlafland@gmail.com>
2015-02-24 01:07:06 -0500

    test

--- WeVue/Production-1.0 copy 2/js/app.js
+++ WeVue/Production-1.0 copy 2/js/app.js
@@ -1,6 +1,66 @@
 
 
+(function() {
+            var quotes = $(".quotes");
+            var quoteIndex = -1;
+            function showNextQuote() {
+                ++quoteIndex;
+                quotes.eq(quoteIndex % quotes.length)
+                    .fadeIn(2000)
+                    .delay(1000)
+                    .fadeOut(2000, showNextQuote);
+            }
+            showNextQuote();
+        })();
+(function() {
+            var quotes = $(".quotes2");
+            var quoteIndex = -1;
+            function showNextQuote() {
+                ++quoteIndex;
+                quotes.eq(quoteIndex % quotes.length)
+                    .fadeIn(2000)
+                    .delay(1000)
+                    .fadeOut(2000, showNextQuote);
+            }
+            showNextQuote();
+        })();
+(function() {
+            var quotes = $(".branding-img");
+            var quoteIndex = -1;
+            function showNextQuote() {
+                ++quoteIndex;
+                quotes.eq(quoteIndex % quotes.length)
+                    .fadeIn(1000)
+                    .delay(4300)
+                    .fadeOut(2000, showNextQuote);
+            }
+            showNextQuote();
+        })();
+(function() {
+            var quotes = $(".features-img");
+            var quoteIndex = -1;
+            function showNextQuote() {
+                ++quoteIndex;
+                quotes.eq(quoteIndex % quotes.length)
+                    .fadeIn(500)
+                    .delay(3200)
+                    .fadeOut(1500, showNextQuote);
+            }
+            showNextQuote();
+        })();
 
+(function() {
+    var quotes = $(".screen-2-fade");
+    var quoteIndex = -1;
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(500)
+            .delay(2000)
+            .fadeOut(500, showNextQuote);
+    }
+    showNextQuote();
+})();
 
 
 onScrollInit( $('.os-animation') );
