13e17a3e167f087bdb00dbc8c105275ba12fa557
jordan lafland <jordantlafland@gmail.com>
2015-02-24 13:33:33 -0500

    test

--- WeVue/Production-1.0/js/app.js
+++ WeVue/Production-1.0/js/app.js
@@ -1,5 +1,30 @@
 
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
 
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// fade in quotes page 1
 (function() {
             var quotes = $(".quotes");
             var quoteIndex = -1;
@@ -12,6 +37,8 @@
             }
             showNextQuote();
         })();
+
+// sldie control on phones 
 (function() {
             var quotes = $(".quotes2");
             var quoteIndex = -1;
@@ -62,7 +89,7 @@
     showNextQuote();
 })();
 
-
+// activates animate.css classes on scroll 
 onScrollInit( $('.os-animation') );
 
 
@@ -95,158 +122,3 @@ function onScrollInit( items, trigger ) {
  onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') );
 
  
-
-    google.maps.event.addDomListener(window, 'load', init);
-    var map;
-    function init() {
-        var mapOptions = {
-            center: new google.maps.LatLng(27.935675,-82.454053),
-            zoom: 12,
-            zoomControl: true,
-            zoomControlOptions: {
-                style: google.maps.ZoomControlStyle.DEFAULT,
-            },
-            disableDoubleClickZoom: true,
-            mapTypeControl: true,
-            mapTypeControlOptions: {
-                style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-            },
-            scaleControl: false,
-            scrollwheel: false,
-            panControl: true,
-            streetViewControl: true,
-            draggable : true,
-            overviewMapControl: true,
-            overviewMapControlOptions: {
-                opened: false,
-            },
-            mapTypeId: google.maps.MapTypeId.ROADMAP,
-            styles: [
-    {
-        "featureType": "landscape",
-        "stylers": [
-            {
-                "hue": "#F1FF00"
-            },
-            {
-                "saturation": -27.4
-            },
-            {
-                "lightness": 9.4
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "stylers": [
-            {
-                "hue": "#0099FF"
-            },
-            {
-                "saturation": -20
-            },
-            {
-                "lightness": 36.4
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "stylers": [
-            {
-                "hue": "#00FF4F"
-            },
-            {
-                "saturation": 0
-            },
-            {
-                "lightness": 0
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "road.local",
-        "stylers": [
-            {
-                "hue": "#FFB300"
-            },
-            {
-                "saturation": -38
-            },
-            {
-                "lightness": 11.2
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "stylers": [
-            {
-                "hue": "#00B6FF"
-            },
-            {
-                "saturation": 4.2
-            },
-            {
-                "lightness": -63.4
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "stylers": [
-            {
-                "hue": "#9FFF00"
-            },
-            {
-                "saturation": 0
-            },
-            {
-                "lightness": 0
-            },
-            {
-                "gamma": 1
-            }
-        ]
-    }
-],
-        }
-        var mapElement = document.getElementById('map');
-        var map = new google.maps.Map(mapElement, mapOptions);
-        var locations = [
-['WeVue', 'undefined', 'undefined', 'undefined', 'undefined', 27.952567, -82.459503, 'https://mapbuildr.com/assets/img/markers/default.png']
-        ];
-        for (i = 0; i < locations.length; i++) {
-            if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
-            if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
-            if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
-           if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
-           if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
-            marker = new google.maps.Marker({
-                icon: markericon,
-                position: new google.maps.LatLng(locations[i][5], locations[i][6]),
-                map: map,
-                title: locations[i][0],
-                desc: description,
-                tel: telephone,
-                email: email,
-                web: web
-            });
-link = '';     }
-
-}
