e35f97973a4ae33ed223fe6d82640f6b4552804a
jordan lafland <jordantlafland@gmail.com>
2015-03-02 19:24:23 -0500

    test

--- workingAngular copy/js/app.js
+++ workingAngular copy/js/app.js
@@ -12,4 +12,19 @@ angular
 			
 		})
 			
+		$stateProvider
+			.state('about', {
+			url: '/about',
+			templateUrl: 'template/about.html',
+			controller: 'aboutCtrl'	
+			
+		})
+
+		$stateProvider
+			.state('contact', {
+			url: '/contact',
+			templateUrl: 'template/contact.html',
+			controller: 'contactCtrl'	
+			
+		})
 	}])
