af86f40140582d82ad879c2fb3cde718738c3e11
jordan lafland <jordantlafland@gmail.com>
2015-03-03 01:51:17 -0500

    test

--- workingAngular copy/js/controller/contactCtrl.js
+++ workingAngular copy/js/controller/contactCtrl.js
@@ -1,7 +1,7 @@
 angular
 	.module('app')
-	.controller('aboutCtrl', ['$scope', function($scope) {
-		$scope.title ="About";
+	.controller('contactCtrl', ['$scope', function($scope) {
+		$scope.title ="contact";
 		$scope.items = ['thing1', 'thing2', 'thing3'];
 		$scope.projects = ['#1','#2','#3'];
 	}]);
