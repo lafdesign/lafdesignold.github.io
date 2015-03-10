2877f2101c72020ffd633f1e6677596bb6bea762
lafdesign <j.t.lafland@gmail.com>
2015-03-10 18:42:07 -0400

    new angular directory

---
+++ angular/js/app.js
@@ -0,0 +1,18 @@
+angular
+  .module('app', [
+    'ui.router'
+  ])
+  .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'home.html',
+        controller: 'homeCtrl'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'about.html',
+        controller: 'aboutCtrl'
+      })
+  }])
