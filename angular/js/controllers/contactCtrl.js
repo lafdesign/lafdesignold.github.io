91338e2bcc64eed2c12fb577cbd2a5a27f88b655
lafdesign <j.t.lafland@gmail.com>
2015-03-10 19:18:43 -0400

    contact ctrl

--- angular/js/controllers/contactCtrl.js
+++ angular/js/controllers/contactCtrl.js
@@ -1,9 +1,9 @@
 angular
   .module('app')
-  .controller('contactCtrl', ['$scope',  function($scope) {
-    $scope.title = "Contact";
+  .controller('contactCtrl', ['$scope', function($scope) {
+  	$scope.title = "Contact";
     $scope.items = ['home','about','contact'];
     $scope.selectedValue = 'contact';
-    
+
     
   }]);
