c6c4afc97e250490c8216f1868f3a02b0379f9cb
lafdesign <j.t.lafland@gmail.com>
2015-03-10 19:13:40 -0400

    fix nav

--- angular/js/controllers/aboutCtrl.js
+++ angular/js/controllers/aboutCtrl.js
@@ -1,6 +1,7 @@
 angular
   .module('app')
   .controller('aboutCtrl', ['$scope', function($scope) {
-  
-    });
+  	$scope.title = "Contact";
+    $scope.items = ['home','about','contact'];
+    $scope.selectedValue = 'contact';
   }]);
--- angular/js/controllers/contactCtrl.js
+++ angular/js/controllers/contactCtrl.js
@@ -1,6 +1,8 @@
 angular
   .module('app')
   .controller('contactCtrl', ['$scope',  function($scope) {
+    $scope.title = "Contact";
+    $scope.items = ['home','about','contact'];
+    $scope.selectedValue = 'contact';
     
-    });
   }]);
