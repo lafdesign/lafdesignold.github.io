8d0ae815d35537c9037723849f76d61bacdbfe3c
Jordan Lafland <lafland.design@gmail.com>
2015-03-12 16:25:54 -0400

    new portfolio!

--- workingAngular/js/controller/homeCtrl.js
+++
@@ -1,8 +0,0 @@
-angular
-	.module('app')
-	.controller('homeCtrl', ['$scope', 'signitures', function($scope, signitures) {
-		$scope.title = "gallery";
-		$scope.signitures = signitures;
-		$scope.items = ['home', 'about', 'contact'];
-		$scope.selectedValue = 'contact';
-	}]);
