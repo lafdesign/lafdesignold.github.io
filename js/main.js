970ae9eb1b21385f6135a24f8dd26fec43026525
Jordan Lafland <lafland.design@gmail.com>
2015-03-17 18:18:34 -0400

    init skrollr

--- js/main.js
+++ js/main.js
@@ -444,3 +444,12 @@ if (typeof jQuery === 'undefined') {
 
         window.addEventListener("scroll", parallax, false);
 
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery );
