7dbd35ebe5a4d423b7b18c55a6a939e14e0d9e04
Jordan Lafland <lafland.design@gmail.com>
2015-03-18 18:58:32 -0400

    updated wevue angular

---
+++ wevue-web/wevue-sass-angular/js/controllers/homeCtrl.js
@@ -0,0 +1,9 @@
+angular
+  .module('app')
+  .controller('homeCtrl', ['$scope', function($scope) {
+    $scope.title = "Home";
+    $scope.items = ['home','about','contact'];
+    $scope.selectedValue = 'home';
+
+  
+  }]);
