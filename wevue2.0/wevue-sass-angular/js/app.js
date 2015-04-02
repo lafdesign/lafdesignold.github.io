f7d8c2b8a0dc28f0fc265006d9724c98dd3ea7b2
Jordan Lafland <lafland.design@gmail.com>
2015-04-02 12:49:26 -0400

    add new wevue file

---
+++ wevue2.0/wevue-sass-angular/js/app.js
@@ -0,0 +1,23 @@
+angular
+  .module('app', [
+    'ui.router'
+  ])
+  .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'templates/about.html',
+        controller: 'aboutCtrl'
+      })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'templates/contact.html',
+        controller: 'contactCtrl'
+      })
+  }])
