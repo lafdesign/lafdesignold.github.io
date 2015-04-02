f74f7102ce404e6882f5c77d463ec5895fce5fee
Jordan Lafland <lafland.design@gmail.com>
2015-04-02 14:27:21 -0400

    no ui router

--- wevue2.0/js/controllers/homeCtrl.js
+++
@@ -1,9 +0,0 @@
-angular
-  .module('app')
-  .controller('homeCtrl', ['$scope', function($scope) {
-    $scope.title = "Home";
-    $scope.items = ['home','about','contact'];
-    $scope.selectedValue = 'home';
-
-  
-  }]);
